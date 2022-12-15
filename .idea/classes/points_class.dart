81e87fba32a861eaf5d632384f45dac4b33ac997
Stuti <stuti_ajmera.sias21@krea.ac.in>
2022-12-15 22:34:20 +0530

    sample commit

---
+++ .idea/classes/points_class.dart
@@ -0,0 +1,11 @@
+void distance(double x, double y) {
+  print("Distance between x and y: ${x.distanceTo(p3)}");
+}
+
+void doubleVal(double x, double y){
+  print("Doubled values ${x*x, y*y}");
+}
+
+double dotProd(double x, double y){
+  double
+}
